076a9659166dcc282e5f22301f008c2e2ff5caae
marrianstjames25 <72973155+marrianstjames25@users.noreply.github.com>
2023-02-06 14:18:48 +0200

    Add project files.

---
+++ LogInDBs/LogIn_.xaml.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Data;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace LogInDBs
+{
+    /// <summary>
+    /// Interaction logic for LogIn_.xaml
+    /// </summary>
+    public partial class LogIn_ : Window
+    {
+        public LogIn_()
+        {
+            InitializeComponent();
+        }
+
+        private void Submit_Click(object sender, RoutedEventArgs e)
+        {
+            SqlConnection sqlCon = new SqlConnection(@"Data Source=(localdb)\MSSQLLocalDB; Initial Catalog=DataLog; Integrated Security=True");
+
+            try
+            {
+                if(sqlCon.State == ConnectionState.Closed)
+                sqlCon.Open();
+                string query = "SELECT COUNT(1) FROM UserData Where Username=@Username and Password=@Password";
+                SqlCommand sqlCmd = new SqlCommand(query, sqlCon);
+                sqlCmd.CommandType = CommandType.Text;
+                sqlCmd.Parameters.AddWithValue("@Username", txtUsername.Text);
+                sqlCmd.Parameters.AddWithValue("@Password", PasswordBox.Password);
+
+                int count = Convert.ToInt32(sqlCmd.ExecuteScalar());
+                if(count == 1)
+                {
+                    MainWindow dashboard = new MainWindow();
+                    dashboard.Show();
+                    this.Close();
+                }
+                else
+                {
+                    MessageBox.Show("Username or password are not correct!");
+                }
+
+
+            }
+            catch (Exception ex)
+            {
+
+                MessageBox.Show(ex.Message);
+            }
+            finally
+            {
+                sqlCon.Close(); 
+            }
+
+
+        }
+    }
+}
