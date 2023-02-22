5c8fe00d6c41c34bf6a467418657528090d4f474
marrianstjames25 <72973155+marrianstjames25@users.noreply.github.com>
2023-02-22 11:55:44 +0200

    Changes Load

---
+++ LogInDBs/Load.xaml.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace LogInDBs
+{
+    /// <summary>
+    /// Interaction logic for Load.xaml
+    /// </summary>
+    public partial class Load : Window
+    {
+        public Load()
+        {
+            InitializeComponent();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+          
+                string dbsCon = @"Data Source=(localdb)\MSSQLLocalDB; Initial Catalog=SignUpDB; Integrated Security=True";
+                SqlConnection sqlCon_ = new SqlConnection(dbsCon);
+
+                try
+                {
+                    sqlCon_.Open();
+                    string query = "Select Email,FirstName from SignupTable_";
+                    SqlCommand cmd = new SqlCommand(query, sqlCon_);
+                    cmd.ExecuteNonQuery();
+                    SqlDataAdapter adapter = new SqlDataAdapter(cmd);
+                    DataTable dt = new DataTable();
+                    adapter.Fill(dt);
+                    DataGrid_.ItemsSource = dt.DefaultView;
+                    adapter.Update(dt);
+
+                    MessageBox.Show("Successful loading");
+                    sqlCon_.Close();
+
+                }
+                catch (Exception ex)
+                {
+
+                    MessageBox.Show(ex.Message);
+                }
+            }
+        }
+    }
+
--- LogInDBs/MainWindow.xaml.cs
+++ LogInDBs/MainWindow.xaml.cs
@@ -1,5 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -24,5 +26,10 @@ namespace LogInDBs
         {
             InitializeComponent();
         }
+
+        
+
+       
+        }
     }
-}
+
